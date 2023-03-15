e37db639bbf8d11a281a2f697f66b29b00f414ff
JonahPeck <PeckJonahC@gmail.com>
2023-03-15 10:06:21 -0600

    solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted(letter for letter in word)
+
+    def match(self, word_list):
+        match_world_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_world_list.append(word)
+
+        return match_world_list
